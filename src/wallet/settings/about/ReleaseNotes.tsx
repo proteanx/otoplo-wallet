304d626e9f492c0d342c56ccf530a2e9e3ee87a3
vgrunner <vgogh241@gmail.com>
2023-12-26 21:44:26 +0200

    bump version and simplify release notes

--- src/wallet/settings/about/ReleaseNotes.tsx
+++ src/wallet/settings/about/ReleaseNotes.tsx
@@ -1,59 +1,30 @@
-import { CapacitorHttp } from "@capacitor/core";
 import { useState } from "react";
-import { Button, Modal, Spinner } from "react-bootstrap";
-
-interface INotes {
-  version: string;
-  feats: string[];
-  fixes: string[];
-}
+import { Button, Modal } from "react-bootstrap";
 
 export default function ReleaseNotes() {
   const [showDialog, setShowDialog] = useState(false);
-  const [notes, setNotes] = useState<INotes>();
-  const [err, setErr] = useState('');
-
-  const fetchNotes = async () => {
-    setErr('');
-    setShowDialog(true)
-    try {
-      if (!notes) {
-        let res = await CapacitorHttp.get({ url: `https://release.otoplo.com/otoplo-wallet/${import.meta.env.VITE_VERSION}/info.json` });
-        if (res.status !== 200) {
-            throw new Error("Failed to fetch notes");
-        }
-        setNotes(res.data);
-      }
-    } catch (e) {
-      console.error(e);
-      setErr("Failed to fetch release notes.");
-    }
-  }
 
   return (
     <>
-      <Button onClick={fetchNotes}>Details</Button>
+      <Button onClick={() => setShowDialog(true)}>Details</Button>
 
       <Modal data-bs-theme='dark' contentClassName='text-bg-dark' show={showDialog} onHide={() => setShowDialog(false)} centered>
         <Modal.Header closeButton>
           <Modal.Title className="ms-auto">Release Notes</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          {notes ? (
-            <>
-              <div className="larger mb-3">Otoplo Wallet {notes.version}</div>
-              <div className="mb-3 smaller">
-                <div className="bold mb-1">NEWS</div>
-                {notes.feats.map((f, i) => <div className='light-txt' key={i}>- {f}</div>)}
-              </div>
-              <div className="mb-1 smaller">
-                <div className="bold mb-1">FIXES</div>
-                {notes.fixes.map((f, i) => <div className='light-txt' key={i}>- {f}</div>)}
-              </div>
-            </>
-          ) : (
-            <div className="center">{err || <Spinner animation="grow"/>}</div>
-          )}
+          <div className="larger mb-3">Otoplo Wallet {import.meta.env.VITE_VERSION}</div>
+          {/* <div className="mb-3 smaller">
+            <div className="bold mb-1">NEWS</div>
+            {notes.feats.map((f, i) => <div className='light-txt' key={i}>- {f}</div>)}
+          </div> */}
+          <div className="mb-3 smaller">
+            <div className="bold mb-1">FIXES</div>
+            <div className='light-txt'>- Fixed crashing issue on old devices</div>
+          </div>
+          <div className='mb-1 smaller url'>
+            <a href='' target='_blank'>Full Release notes <i className="va fa-solid fa-arrow-up-right-from-square fa-xs"/></a>
+          </div>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={() => setShowDialog(false)}>Close</Button> 
