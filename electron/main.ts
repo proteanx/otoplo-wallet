d128785b78b7321cf5a4bd04cacb7f145d9ba102
Proteus <pro@proteanx.com>
2024-08-16 00:18:53 -0400

    clean up build (appimage)

--- electron/main.ts
+++ electron/main.ts
@@ -1,7 +1,7 @@
 import { app, shell, BrowserWindow, ipcMain } from 'electron';
 import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
-import { checkForUpdates, exportFile } from './utils.js';
+import { checkForUpdates, exportFile } from './utils';
 
 let mainWindow: BrowserWindow;
 const gotTheLock = app.requestSingleInstanceLock();
